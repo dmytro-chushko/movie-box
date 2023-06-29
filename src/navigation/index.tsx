8f86696d0d96367aa7e19f1b99f5e26da8c86dd9
dmytro-chushko <dmytro.chushko@gmail.com>
2023-06-29 23:13:49 +0300

    feat: Add route for movie details

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,13 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { SharedLayout } from '../components/shared-layout';
-import { Home } from '../pages/home';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { SharedLayout } from "../components/shared-layout";
+import { Home } from "../pages/home";
+import { Details } from "pages/details";
 
 export const MainRouter = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<SharedLayout />}>
-        <Route index element={<Home />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+	<BrowserRouter>
+		<Routes>
+			<Route path="/" element={<SharedLayout />}>
+				<Route index element={<Home />} />
+				<Route path="/:id" element={<Details />} />
+			</Route>
+		</Routes>
+	</BrowserRouter>
 );
